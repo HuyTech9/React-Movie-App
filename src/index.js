356d4816d0b9cfa989fb492be4f707a11f4e1ab2
HuyTech9 <khanghuy9979@gmail.com>
2023-05-20 18:42:00 +0300

    project done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
+//**Source Code: JavaScript Mastery, React JS Full Course 2023 | Build an App and Master React in 1 Hour */
+
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
